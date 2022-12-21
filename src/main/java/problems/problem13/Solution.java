854d5aa674427c6699dd6edeb9fc93634c502f69
AyyubxonRustamov <rustamovayyubxon@gmail.com>
2022-12-21 09:35:47 +0500

    Problem 13

--- src/main/java/problems/problem13/Solution.java
+++ src/main/java/problems/problem13/Solution.java
@@ -1,6 +1,8 @@
 package problems.problem13;
 
 /*
+    13. Roman to Integer
+
     Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
     Symbol       Value
--- src/test/java/problems/problem13/SolutionTest.java
+++ src/test/java/problems/problem13/SolutionTest.java
@@ -8,31 +8,31 @@ class SolutionTest {
 
     @Test
     void III_shouldReturn_three() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem13
         assertEquals(3, solution.romanToInt("III"));
     }
 
     @Test
     void LVIII_shouldReturn_fiftyEight() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem13
         assertEquals(58, solution.romanToInt("LVIII"));
     }
 
     @Test
     void MCMXCIV_shouldReturn_oneThousandNineHundredNinetyFour() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem13
         assertEquals(1994, solution.romanToInt("MCMXCIV"));
     }
 
     @Test
     void XIX_shouldReturn_twentyNine() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem13
         assertEquals(19, solution.romanToInt("XIX"));
     }
 
     @Test
     void I_shouldReturn_one() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem13
         assertEquals(1, solution.romanToInt("I"));
     }
 }
