6e54a98eee4e2f9657e5f29f23fb32b1b9f4c059
AyyubxonRustamov <rustamovayyubxon@gmail.com>
2022-12-21 09:36:59 +0500

    Problem 14

--- src/test/java/problems/problem14/SolutionTest.java
+++ src/test/java/problems/problem14/SolutionTest.java
@@ -8,21 +8,21 @@ class SolutionTest {
 
     @Test
     void flowerFlowFlight_shouldReturn_fl() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem14
         String[] strs = {"flower", "flow", "flight"};
         assertEquals("fl", solution.longestCommonPrefix(strs));
     }
 
     @Test
     void dogRacecarCar_shouldReturn_emptyString() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem14
         String[] strs = {"dog", "racecar", "car"};
         assertEquals("", solution.longestCommonPrefix(strs));
     }
 
     @Test
     void emptyArray_shouldReturn_emptyString() {
-        var solution = new Solution();
+        var solution = new Solution(); //problems.problem14
         String[] strs = {};
         assertEquals("", solution.longestCommonPrefix(strs));
     }
