4dddd92dd60a7bec7ea383e63a856b03d60c2165
AyyubxonRustamov <rustamovayyubxon@gmail.com>
2022-12-27 21:30:10 +0500

    Problem 20

---
+++ src/main/java/problems/problem20/Solution.java
@@ -0,0 +1,47 @@
+package problems.problem20;
+
+/*
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string
+    is valid.
+    An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+    Example 1:
+
+    Input: s = "()"
+    Output: true
+    Example 2:
+
+    Input: s = "()[]{}"
+    Output: true
+    Example 3:
+
+    Input: s = "(]"
+    Output: false
+
+    Constraints:
+
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+
+    Author: Rustamov Ayyubxon | ayyubxonmaverick@gmail.com
+*/
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+}
