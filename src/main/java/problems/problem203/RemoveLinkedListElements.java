0ced025af6eab38eff311f3c8553f6325966ea10
AyyubxonRustamov <rustamovayyubxon@gmail.com>
2022-12-31 00:39:02 +0500

    Problem 203

--- src/main/java/problems/problem203/RemoveLinkedListElements.java
+++ src/main/java/problems/problem203/RemoveLinkedListElements.java
@@ -1,6 +1,8 @@
 package problems.problem203;
 
 /*
+    203. Remove Linked List Elements
+
     Given the head of a linked list and an integer val, remove all the nodes of the linked list that has
     Node.val == val, and return the new head.
 
@@ -29,7 +31,7 @@ package problems.problem203;
 
 import problems.ListNode;
 
-public class Solution {
+public class RemoveLinkedListElements {
 
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
