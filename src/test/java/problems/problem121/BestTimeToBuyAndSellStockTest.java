3cbba15db5524d59a8f913415f72827368e293a2
AyyubxonRustamov <rustamovayyubxon@gmail.com>
2023-01-03 20:45:07 +0500

    Problem 121

--- src/test/java/problems/problem121/BestTimeToBuyAndSellStockTest.java
+++ src/test/java/problems/problem121/BestTimeToBuyAndSellStockTest.java
@@ -1,7 +1,8 @@
 package problems.problem121;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BestTimeToBuyAndSellStockTest {
 
@@ -15,14 +16,14 @@ class BestTimeToBuyAndSellStockTest {
     @Test
     void array2_shouldReturn_zero() {
         var bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
-        int[] array2 = {7,6,4,3,1};
+        int[] array2 = {7, 6, 4, 3, 1};
         assertEquals(0, bestTimeToBuyAndSellStock.maxProfit(array2));
     }
 
     @Test
     void array3_shouldReturn_two() {
         var bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
-        int[] array3 = {2,4,1};
+        int[] array3 = {2, 4, 1};
         assertEquals(2, bestTimeToBuyAndSellStock.maxProfit(array3));
     }
 }
