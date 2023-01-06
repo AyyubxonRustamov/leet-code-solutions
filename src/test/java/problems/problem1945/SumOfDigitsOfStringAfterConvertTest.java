86a48b1e3fe7450877d73b8d6a700a7c24d1eed3
AyyubxonRustamov <rustamovayyubxon@gmail.com>
2023-01-07 00:06:11 +0500

    Problem 1945

---
+++ src/test/java/problems/problem1945/SumOfDigitsOfStringAfterConvertTest.java
@@ -0,0 +1,26 @@
+package problems.problem1945;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumOfDigitsOfStringAfterConvertTest {
+
+    @Test
+    void getLucky_s_equals_iiii_k_equals_one_shouldReturn_thirtySix() {
+        var sum = new SumOfDigitsOfStringAfterConvert(); //problems.problem1945
+        assertEquals(36, sum.getLucky("iiii", 1));
+    }
+
+    @Test
+    void getLucky_s_equals_leetcode_k_equals_two_shouldReturn_six() {
+        var sum = new SumOfDigitsOfStringAfterConvert(); //problems.problem1945
+        assertEquals(6, sum.getLucky("leetcode", 2));
+    }
+
+    @Test
+    void getLucky_s_equals_zbax_k_equals_two_shouldReturn_eight() {
+        var sum = new SumOfDigitsOfStringAfterConvert(); //problems.problem1945
+        assertEquals(8, sum.getLucky("zbax", 2));
+    }
+}
